bb3a10fbfcfbc4d8a8b76dd6463c161a5fe6cd32
HonzaJazz <honzajazz@gmail.com>
2015-02-16 06:56:14 +0100

    fix gpx loading function

--- showtrack.js
+++ showtrack.js
@@ -19,18 +19,27 @@
 	return m;
 }
 
-function showGpx(url) {
-	var xhr = new JAK.Request(JAK.Request.XML);
-	xhr.setCallback(window, "response");
-	xhr.send(url);
+function showGPXDoc(url){
+	var xmlhttp;
+	if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+			var xmlSrc=xmlhttp.responseXML;
+			var gpx = new SMap.Layer.GPX(xmlSrc);
+			map.addLayer(gpx);
+			gpx.enable();
+			gpx.fit();
+		}
+	}
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
 }
 
-function response(xmlDoc) {
-    var gpx = new SMap.Layer.GPX(xmlDoc);
-    map.addLayer(gpx);
-    gpx.enable();
-    gpx.fit();
-}
 
 map = mapInit("m");
-showGpx(location.search.substring(1));
+showGPXDoc(location.search.substring(1));
